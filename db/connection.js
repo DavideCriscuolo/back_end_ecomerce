0ad08654aea0973a8bff0cb3e5668dd61a6576ac
DavideCriscuolo <davide.alcatel@gmail.com>
2025-09-02 19:59:37 +0200

    creata connessione al server

---
+++ db/connection.js
@@ -0,0 +1,22 @@
+import mysql from "mysql2";
+
+const credentials = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PW,
+  database: process.env.DB_NAME,
+};
+
+const connection = mysql.createConnection(credentials);
+
+console.log(connection);
+
+connection.connect((err) => {
+  if (err) {
+    console.log(err.message);
+  } else {
+    console.log("Connection Success");
+  }
+});
+
+export default connection;
